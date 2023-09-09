3dd556ab57be13ac3f911f2912cb5a72eca05d07
Maximilian Leith <maximilian@leith.de>
2023-09-09 09:36:34 +0200

    include gcgl when evaluating the connection state

--- src/airportExpressAccessory.ts
+++ src/airportExpressAccessory.ts
@@ -224,7 +224,14 @@ not responding since ${secondsOffline} seconds.`,
             `${this.accessory.context.device.displayName} - Update: Bit 11 is "${bit11}"`,
         );
 
-        if (bit11 === false) {
+        const gcgl: boolean = mDNS_TXT_record
+            .find((r: string) => r.indexOf('gcgl') > -1)!
+            .replace('gcgl=', '') === '1';
+        this.platform.log.debug(
+            `${this.accessory.context.device.displayName} - Update: gcgl is "${gcgl}"`,
+        );
+
+        if ((gcgl && bit11) || !bit11) {
             return this.platform.Characteristic.OccupancyDetected
                 .OCCUPANCY_NOT_DETECTED;
         } else {
